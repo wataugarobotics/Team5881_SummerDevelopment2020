0d4ccf3357c6591efdc7d36638b5531209e8d93c
QwertyLD <lukedoty03@gmail.com>
2020-06-19 11:03:24 -0400

    Added a OnePersonTeleOP class. Changed startPressed variable to stickPressed in both teleOP classes to better reflect controller action.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OnePersonTeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OnePersonTeleOP.java
@@ -2,14 +2,10 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.util.Range;
 
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.teamcode.HardwareAndMethods;
-
-@TeleOp(name="2p TeleOP", group="Tungsteel 2019-20")
+@TeleOp(name="1p TeleOP", group="Tungsteel 2019-20")
 //@Disabled
-public class TwoPersonTeleOP extends OpMode {
+public class OnePersonTeleOP extends OpMode {
     //Create HardwareAndMethods instance called robot
     private HardwareAndMethods robot = new HardwareAndMethods();
 
@@ -78,18 +74,18 @@ public class TwoPersonTeleOP extends OpMode {
         } else if(!gamepad1.left_bumper) parkingChanged = false;
 
         //Claw servo
-        if(gamepad2.x && !clawChanged) {
+        if(gamepad1.x && !clawChanged) {
             robot.claw.setPosition(clawOn ? 0 : 1);
             clawOn = !clawOn;
             clawChanged = true;
-        } else if(!gamepad2.x) clawChanged = false;
+        } else if(!gamepad1.x) clawChanged = false;
 
         //Capstone servo
-        if(gamepad2.b && !capstoneChanged) {
+        if(gamepad1.b && !capstoneChanged) {
             robot.capstone.setPosition(capstoneOn ? 0 : 1);
             capstoneOn = !capstoneOn;
             capstoneChanged = true;
-        } else if(!gamepad2.b) capstoneChanged = false;
+        } else if(!gamepad1.b) capstoneChanged = false;
 
 
         // Calls mechanum method
@@ -97,7 +93,7 @@ public class TwoPersonTeleOP extends OpMode {
         robot.mechanum(-gamepad1.left_stick_x, gamepad1.left_stick_y, -gamepad1.right_stick_x);
 
         //Calls lift method
-        robot.lift(gamepad2.right_trigger - gamepad2.left_trigger);
+        robot.lift(gamepad1.right_trigger - gamepad1.left_trigger);
     }
 
     /*
